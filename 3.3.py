fb5331c1798bce96a1f87da7e029cba38c5e6fe5
sergiifartushnyi <sergiyfartyshnyy@gmail.com>
2024-07-29 13:14:35 +0300

    3.3

---
+++ 3.3.py
@@ -0,0 +1,19 @@
+a = ([1,2,3,4,5,6])
+list_1 = [a[0:3],a[-3:]]
+print (list_1)
+
+a = ([1,2,3])
+list_2 = [a[0:2],a[-1:]]
+print (list_2)
+
+a = ([1,2,3,4,5])
+list_3 = [a[0:3],a[-2:]]
+print (list_3)
+
+a = ([1,])
+list_4 = [a[0:],[]]
+print (list_4)
+
+a = [[],[]]
+list_5 = a
+print (list_5)
